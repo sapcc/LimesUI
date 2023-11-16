c31a9db462844d36c5dea2a96369d2dd406aa4cd
Martin David Schulze <martin.david.schulze@sap.com>
2023-11-16 11:15:38 +0100

    Bar UI 2 (#20)

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { t, sortByLogicalOrderAndName, tracksQuota } from "../lib/utils";
-import ProjectResource from "./project/ProjectResource";
+import React from "react"
+import { t, sortByLogicalOrderAndName, tracksQuota } from "../lib/utils"
+import ProjectResource from "./project/ProjectResource"
 
 const categoryTitle = `
     text-lg 
@@ -8,33 +8,33 @@ const categoryTitle = `
     font-bold 
     col-span-full 
     text-theme-hig
-    `;
+    `
 const categoryContent = `
     grid
     auto-rows-fr
     gap-5 
-    grid-cols-[repeat(auto-fit,_minmax(31rem,_1fr))]
-    `;
+    grid-cols-[repeat(auto-fit,_minmax(37rem,_1fr))]
+    `
 
 // TODO: for domain/cluster level add tracksQuota which skips resource that do not track quota.
 
 const Category = (props) => {
-  const { categoryName, canEdit } = props;
-  const { area, resources } = props.category;
+  const { categoryName, canEdit } = props
+  const { area, resources } = props.category
   const forwardProps = {
     categoryName,
     area,
     canEdit,
-  };
+  }
 
   //for usage-only resources with no quota of their own, this finds
   //the resource they're ultimately "contained_in"
   function getContainingResourceFor(resName) {
-    const res = resources.find((res) => res.name === resName);
+    const res = resources.find((res) => res.name === resName)
     if (res.contained_in) {
-      return getContainingResourceFor(res.contained_in);
+      return getContainingResourceFor(res.contained_in)
     }
-    return res;
+    return res
   }
 
   return (
@@ -54,7 +54,7 @@ const Category = (props) => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Category;
+export default Category
--- src/components/ResourceBar.js
+++ src/components/ResourceBar.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Stack } from "juno-ui-components";
+import React, { useState, useEffect, useRef } from "react"
+import { Stack } from "juno-ui-components"
 
-const LABEL_MARGIN = 10;
-const resourceBarWrapper = ``;
+const LABEL_MARGIN = 10
+const resourceBarWrapper = ``
 
 const barConainer = `
   mb-2 
   min-w-full 
   gap-1
-`;
+`
 
 const baseResourceBar = `
   rounded 
@@ -17,41 +17,51 @@ const baseResourceBar = `
   flex 
   p-0.5
   h-8
-`;
+`
 const emptyResourceBar = `
   bg-theme-background-lvl-2 
-  `;
+  `
 const filledResourceBar = `
   text-white 
   bg-sap-blue-3 
   has-label-if-fits 
   rounded-sm 
-  `;
+  `
 const emptyExtraResourceBar = `
   bg-theme-background-lvl-4 
 
-  `;
+  `
 const filledExtraResourceBar = `
   text-white 
   bg-sap-purple-2 
   has-label-if-fits 
   rounded-sm  
-  `;
+  `
 const noneResourceBar = `
   rounded 
   border 
-  border-theme-background-lvl-4 
-  px-2 
-  font-bold 
+  border-theme-background-lvl-4
+  flex 
+  p-0.5
+  h-8
   text-theme-light 
   italic
-  `;
+  `
 const barLable = `
   px-1
-`;
+  text-sm
+  font-bold
+`
+const disabledLable = `
+  text-theme-light 
+  italic
+  px-1
+  text-sm
+  font-bold
+`
 
 const ResourceBar = (props) => {
-  const outerDivRef = React.useRef(null);
+  const outerDivRef = React.useRef(null)
   const {
     fillLabel,
     capacityLabel,
@@ -64,43 +74,43 @@ const ResourceBar = (props) => {
     labelIsUsageOnly,
     canEdit,
     showsCapacity,
-  } = props;
+  } = props
 
-  const disabled = false;
+  const disabled = false
 
   React.useLayoutEffect(() => {
-    checkIfLabelFits();
-  });
+    checkIfLabelFits()
+  })
 
   function checkIfLabelFits(opts = {}) {
-    const bar = outerDivRef.current; //this is the <div class="progress"/>
+    const bar = outerDivRef.current //this is the <div class="progress"/>
     if (!bar) {
-      return;
+      return
     }
 
     //measure the width of the filled portion of the bar
-    const filledBar = bar.querySelector(".has-label-if-fits");
+    const filledBar = bar.querySelector(".has-label-if-fits")
     if (!filledBar) {
       //bar is completely full or empty, so we don't have to measure anything
-      return;
+      return
     }
-    const barWidth = filledBar.getBoundingClientRect().width;
+    const barWidth = filledBar.getBoundingClientRect().width
 
     //measure the width of the label (one of them might be display:none and report width=0)
     const labelWidths = [...bar.querySelectorAll(".progress-bar-label")].map(
       (span) => span.getBoundingClientRect().width
-    );
-    const labelWidth = Math.max(...labelWidths);
+    )
+    const labelWidth = Math.max(...labelWidths)
 
     //require some extra wiggle room (in px) around the label to account for UI
     //margins, and because labels that fit too tightly look dumb
     bar
       .querySelector(".progress-bar-label")
-      .classList.toggle("label-fits", labelWidth + LABEL_MARGIN < barWidth);
+      .classList.toggle("label-fits", labelWidth + LABEL_MARGIN < barWidth)
 
     //re-run this method after animations have completed
     if (!opts.delayed) {
-      window.setTimeout(() => checkIfLabelFits({ delayed: true }), 500);
+      window.setTimeout(() => checkIfLabelFits({ delayed: true }), 500)
     }
   }
 
@@ -108,45 +118,59 @@ const ResourceBar = (props) => {
     // First handle the creation of an empty bar.
     if (capacity == 0 && fill == 0) {
       return (
-        <div key="filled" className={noneResourceBar} style={{ width: "100%" }}>
-          <span className="progress-bar-label">
+        <Stack
+          direction="vertical"
+          distribution="between"
+          style={commitment > 0 ? { width: "70%" } : { width: "100%" }}
+        >
+          <span className={`progress-bar-label ${disabledLable}`}>
             {showsCapacity ? "No capacity" : "No quota"}
           </span>
-        </div>
-      );
+          <div
+            key="filled"
+            className={noneResourceBar}
+            style={{ width: "100%" }}
+          ></div>
+        </Stack>
+      )
     }
 
-    let widthPercent = Math.round(1000 * (fill / capacity)) / 10;
+    let widthPercent = Math.round(1000 * (fill / capacity)) / 10
     // ensure that a non-zero-wide bar is at least somewhat visible
     if (fill > 0 && widthPercent < 0.5) {
-      widthPercent = 0.5;
+      widthPercent = 0.5
     }
     let widthCommitment =
-      Math.round((1000 * (fill - capacity)) / extraCapacityValue) / 10;
+      Math.round((1000 * (fill - capacity)) / extraCapacityValue) / 10
+
     //special cases: purple
     let className =
       commitment > 0 || labelIsUsageOnly || !canEdit
         ? "progress-bar"
-        : "progress-bar bg-sap-purple-2";
+        : "progress-bar bg-sap-purple-2"
 
     const label = (
       <span className={`progress-bar-label ${barLable}`}>
         {fillLabel}/{capacityLabel}{" "}
-        {commitment > 0 ? "committed" : "quota used"}
+        {commitment > 0 ? (
+          <span className="font-normal">committed</span>
+        ) : (
+          <span className="font-normal">quota used</span>
+        )}
       </span>
-    );
+    )
 
     const extraLable = (
       <span className={`progress-bar-label ${barLable}`}>
         {extraFillLabel}/{extraCapacityLabel}
       </span>
-    );
+    )
 
-    let filled = className;
-    let barStyleFilled = { width: widthPercent + "%" };
-    let barStyleCommitment = { width: widthCommitment + "%" };
+    let filled = className
+    let barStyleFilled = { width: widthPercent + "%" }
+    let barStyleCommitment = { width: widthCommitment + "%" }
     if (disabled) {
-      filled = "progress-bar progress-bar-disabled has-label";
+      filled = "progress-bar progress-bar-disabled has-label"
     }
 
     const resourceBar = (
@@ -154,37 +178,45 @@ const ResourceBar = (props) => {
         distribution="between"
         className={`process-bar-container ${barConainer}`}
       >
-        <div
-          className={`main-bar ${baseResourceBar} ${emptyResourceBar}`}
-          style={commitment > 0 ? { width: "50%" } : { width: "100%" }}
+        <Stack
+          direction="vertical"
+          distribution="between"
+          style={commitment > 0 ? { width: "70%" } : { width: "100%" }}
         >
-          <div
-            key="filled"
-            className={`main-fill ${filled} ${filledResourceBar}`}
-            style={fill > 0 ? barStyleFilled : { width: "0%" }}
-          ></div>
           {label}
-        </div>
+          <div className={`main-bar ${baseResourceBar} ${emptyResourceBar}`}>
+            <div
+              key="filled"
+              className={`main-fill ${filled} ${filledResourceBar}`}
+              style={fill > 0 ? barStyleFilled : { width: "0%" }}
+            ></div>
+          </div>
+        </Stack>
 
         {commitment ? (
-          <div
-            className={`extra-bar ${baseResourceBar} ${emptyExtraResourceBar}`}
-            style={{ width: "50%" }}
+          <Stack
+            direction="vertical"
+            distribution="between"
+            style={{ width: "30%" }}
           >
-            <div
-              key="extra-filled"
-              className={`extra-fill ${filled} ${filledExtraResourceBar}`}
-              style={fill > capacity ? barStyleCommitment : { width: "0%" }}
-            ></div>
             {extraLable}
-          </div>
+            <div
+              className={`extra-bar ${baseResourceBar} ${emptyExtraResourceBar}`}
+            >
+              <div
+                key="extra-filled"
+                className={`extra-fill ${filled} ${filledExtraResourceBar}`}
+                style={fill > capacity ? barStyleCommitment : { width: "0%" }}
+              ></div>
+            </div>
+          </Stack>
         ) : (
           ""
         )}
       </Stack>
-    );
+    )
 
-    return resourceBar;
+    return resourceBar
   }
 
   return (
@@ -201,7 +233,7 @@ const ResourceBar = (props) => {
         {buildResourceBar()}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default ResourceBar;
+export default ResourceBar
