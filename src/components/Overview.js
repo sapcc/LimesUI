bd477e254bfcc56607305a56af4d78722683ac8d
VoigtS <s.voigt@sap.com>
2023-11-30 09:49:47 +0100

    Overview: Remove padding on Y axis

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -58,7 +58,7 @@ const Overview = (props) => {
   }
 
   return (
-    <Container px={false} className="mb-11 py-6">
+    <Container px={false} className="mb-11">
       <Tabs selectedIndex={currentTabIdx} onSelect={() => {}}>
         <TabList>
           {allAreas.map((area) => (
