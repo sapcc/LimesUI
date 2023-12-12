aee10fc8a4473a31190e6ff3e15405c3377f1453
Stefan Voigt <s.voigt@sap.com>
2023-12-12 19:10:17 +0100

    Add Cypress e2e test (#56)

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe("template spec", () => {
+  it("checks that edit exsists", () => {
+    cy.visit("/");
+    cy.contains("Edit").should('exist')
+  })
+  it("saves amount after modal exit", () => {
+    cy.visit("/");
+    cy.contains("Edit").click();
+    cy.contains("Add Commitment").click();
+    cy.get("input").type("20");
+    cy.get(".juno-select-toggle").click();
+    cy.get(".juno-select-option").first().click();
+    cy.contains("Save").click();
+    cy.get(".juno-modal-container").contains("Cancel").click();
+    cy.get("input").should('have.value', "020");
+  });
+});
