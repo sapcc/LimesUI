7b1cf0942b26c4516f34db81d2f1538f6bd108f8
VoigtS <s.voigt@sap.com>
2023-12-13 10:52:38 +0100

    Cypress: Add Panel Close test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,17 @@
 /// <reference types="Cypress" />
 
+// Warning: All gets with a dot search for a classname in juno
+// Those are inaccessible in our application
+// They might brick at some point in the future.
 describe("template spec", () => {
-  it("checks that edit exsists", () => {
+  const amount = 20;
+  beforeEach(() => {
     cy.visit("/");
+  })
+  it("checks that edit exsists", () => {
     cy.get("[data-cy='edit/instances']").should("exist");
   });
   it("saves amount after modal exit", () => {
-    const amount = 20;
-    cy.visit("/");
     cy.get("[data-cy='edit/instances']").click();
     cy.get("[data-cy='addCommitment']").click();
     cy.get("[data-cy='commitmentInput']").clear().type(amount);
@@ -17,4 +21,16 @@ describe("template spec", () => {
     cy.get("[data-cy='modalCancel']").click();
     cy.get("[data-cy='commitmentInput']").should("have.value", amount);
   });
+
+  it("resets commitment after panel close", () => {
+    cy.get("[data-cy='edit/instances']").click();
+    cy.get("[data-cy='addCommitment']").click();
+    cy.get("[data-cy='commitmentInput']").clear().type(amount);
+    cy.get("[data-cy='commitmentSelect']").click();
+    cy.get("[data-cy='commitmentSelectOption/0']").first().click();
+    cy.get(".juno-panel-close").click()
+    cy.get("[data-cy='edit/instances']").click();
+    cy.get("[data-cy='addCommitment']").click();
+    cy.get("[data-cy='commitmentInput']").should("have.value", 0);
+  })
 });
