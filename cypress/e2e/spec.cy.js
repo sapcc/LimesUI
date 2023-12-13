78ae0bc3300caedcdaa46bec0009514ff76f5038
VoigtS <s.voigt@sap.com>
2023-12-13 09:52:43 +0100

    Cypress: Clear input field before typing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,11 +9,11 @@ describe("template spec", () => {
     cy.visit("/");
     cy.contains("Edit").click();
     cy.contains("Add Commitment").click();
-    cy.get("input").type("20");
+    cy.get("input").clear().type("20");
     cy.get(".juno-select-toggle").click();
     cy.get(".juno-select-option").first().click();
     cy.contains("Save").click();
     cy.get(".juno-modal-container").contains("Cancel").click();
-    cy.get("input").should('have.value', "020");
+    cy.get("input").should('have.value', "20");
   });
 });
