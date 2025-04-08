dc99a1316de2d0e1f1f716fa5adde5d771b74736
VoigtS <s.voigt@sap.com>
2025-04-08 16:49:15 +0200

    implement modal

--- src/components/shared/MergeCommitments.js
+++ src/components/shared/MergeCommitments.js
@@ -3,7 +3,7 @@ import { Button, Icon, Stack } from "@cloudoperators/juno-ui-components";
 
 const MergeCommitment = (props) => {
   const { mergeOps } = props;
-  const { isMerging, setIsMerging, commitmentsToMerge, setCommitmentsToMerge } = mergeOps;
+  const { isMerging, setIsMerging, commitmentsToMerge, setCommitmentsToMerge, setConfirmMerge } = mergeOps;
 
   return (
     <Stack gap="1">
@@ -19,7 +19,15 @@ const MergeCommitment = (props) => {
         <Icon className="mr-2" icon={isMerging ? "cancel" : "autoAwesomeMotion"} title="Receive" size="18" />
         <span>Merge</span>
       </Button>
-      <Button size="small" icon="success" disabled={commitmentsToMerge.length < 2} variant="primary" />
+      <Button
+        size="small"
+        icon="success"
+        disabled={commitmentsToMerge.length < 2}
+        variant="primary"
+        onClick={() => {
+          setConfirmMerge(true);
+        }}
+      />
     </Stack>
   );
 };
