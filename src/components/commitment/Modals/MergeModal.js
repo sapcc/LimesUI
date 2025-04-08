a8da087808bdd04826a9cec3a357cc7d6e7d6a27
VoigtS <s.voigt@sap.com>
2025-04-08 17:03:19 +0200

    implement modal

---
+++ src/components/commitment/Modals/MergeModal.js
@@ -0,0 +1,62 @@
+import React from "react";
+import BaseFooter from "./BaseComponents/BaseFooter";
+import useConfirmInput from "./BaseComponents/useConfirmInput";
+import { Modal, DataGrid, DataGridRow, DataGridCell } from "@cloudoperators/juno-ui-components";
+import { Unit, valueWithUnit } from "../../../lib/unit";
+import { parseCommitmentDuration } from "../../../lib/parseCommitmentDurations";
+
+const label = "font-semibold";
+
+const MergeModal = (props) => {
+  const { commitments = [] } = props;
+  const unit = new Unit(commitments[0].unit);
+  const { action, title, subText, onModalClose } = props;
+  const { ConfirmInput, inputProps, checkInput } = useConfirmInput({ confirmationText: subText });
+  const [mergeAmount, highestDuration] = React.useMemo(() => {
+    let mergeAmount = 0;
+    let highestDuration = "";
+    for (const c of commitments) {
+      mergeAmount = mergeAmount + c.amount;
+      if (parseCommitmentDuration(c.duration) > parseCommitmentDuration(highestDuration)) {
+        highestDuration = c.duration;
+      }
+    }
+    return [mergeAmount, highestDuration];
+  }, [commitments]);
+
+  function onRenew() {
+    action(commitments);
+  }
+
+  return (
+    <Modal
+      className="max-h-full"
+      title={title}
+      open={true}
+      modalFooter={
+        <BaseFooter onModalClose={onModalClose} guardFns={[checkInput]} actionFn={onRenew} variant={"primary"} />
+      }
+      onCancel={() => {
+        onModalClose();
+      }}
+    >
+      <DataGrid columns={2} columnMaxSize="1fr">
+        <DataGridRow>
+          <DataGridCell className={label}>Resource:</DataGridCell>
+          <DataGridCell data-testid={"renewResource"}>{commitments[0].resource_name}</DataGridCell>
+        </DataGridRow>
+        <DataGridRow>
+          <DataGridCell className={label}>Amount:</DataGridCell>
+          <DataGridCell>{valueWithUnit(mergeAmount, unit)}</DataGridCell>
+        </DataGridRow>
+        <DataGridRow>
+          <DataGridCell className={label}>Duration</DataGridCell>
+          <DataGridCell>{highestDuration}</DataGridCell>
+        </DataGridRow>
+      </DataGrid>
+      <ConfirmInput subText={subText} {...inputProps} />
+    </Modal>
+  );
+};
+
+export default MergeModal;
