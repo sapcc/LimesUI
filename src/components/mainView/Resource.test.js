e979817588cab109e12fba78dfd3069a99a53e91
VoigtS <s.voigt@sap.com>
2025-04-28 16:17:23 +0200

    add missing test file

---
+++ src/components/mainView/Resource.test.js
@@ -0,0 +1,90 @@
+import React from "react";
+import Resource from "./Resource";
+import { Scope } from "../../lib/scope";
+import { MemoryRouter } from "react-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { act, renderHook, screen, waitFor } from "@testing-library/react";
+import { PortalProvider } from "@cloudoperators/juno-ui-components";
+import StoreProvider, { globalStore, globalStoreActions } from "../StoreProvider";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
+queryClient.setQueryDefaults(["maxQuota"], {
+  queryFn: () => {},
+});
+
+describe("Resource tests", () => {
+  test("display correct edit option for resource", async () => {
+    let scope = new Scope({ projectID: "123", domainID: "456" });
+    const res = {
+      name: "testResource",
+      quota: 500,
+      capacity: 0,
+      totalCommitments: 10,
+      usagePerCommitted: 5,
+      usagePerQuota: 0,
+      editableResource: false,
+      per_az: [["az1", { projects_usage: 10 }]],
+    };
+    const forwardProps = {
+      area: "testArea",
+      canEdit: true,
+      categoryName: "testCategory",
+      serviceType: "testService",
+    };
+    const wrapper = ({ children }) => (
+      <PortalProvider>
+        <StoreProvider>
+          <QueryClientProvider client={queryClient}>
+            <MemoryRouter>
+              <Resource key={res.name} resource={res} {...forwardProps} tracksQuota={true} />
+              {children}
+            </MemoryRouter>
+          </QueryClientProvider>
+        </StoreProvider>
+      </PortalProvider>
+    );
+    const { result, rerender } = await waitFor(() => {
+      return renderHook(
+        () => ({
+          globalStore: globalStore(),
+          globalStoreActions: globalStoreActions(),
+        }),
+        { wrapper }
+      );
+    });
+    // Project level
+    act(() => {
+      result.current.globalStoreActions.setScope(scope);
+    });
+    expect(screen.getByTestId("maxQuotaEdit")).toBeInTheDocument();
+    // resource does not allow commitments
+    res.editableResource = true;
+    rerender();
+    act(() => {
+      result.current.globalStoreActions.setScope(scope);
+    });
+    expect(screen.queryByTestId("maxQuotaEdit")).not.toBeInTheDocument();
+    expect(screen.getByTestId("edit/testResource")).toBeInTheDocument();
+    expect(screen.queryByTestId("setMaxQuotaPanel")).not.toBeInTheDocument();
+
+    // Domain level
+    scope = new Scope({ domainID: "456" });
+    act(() => {
+      result.current.globalStoreActions.setScope(scope);
+    });
+    expect(screen.getByTestId("setMaxQuotaPanel")).toBeInTheDocument();
+
+    // Cluster level
+    scope = new Scope({});
+    act(() => {
+      result.current.globalStoreActions.setScope(scope);
+    });
+    expect(screen.getByTestId("setMaxQuotaPanel")).toBeInTheDocument();
+  });
+});
