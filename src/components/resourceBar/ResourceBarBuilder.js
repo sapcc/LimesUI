aae73fe7354ba4989fc823df8bd8f81de07a4ab9
VoigtS <s.voigt@sap.com>
2025-06-26 13:31:39 +0200

    Add remaining info texts instead of checking for parent resources, provide the az and keep the 'resource' value consistent. fix: max_quota not visible if its 0 and no edit permissions are present.

--- src/components/resourceBar/ResourceBarBuilder.js
+++ src/components/resourceBar/ResourceBarBuilder.js
@@ -19,7 +19,7 @@ import ResourceBar, { getAppliedBarColors } from "./ResourceBar";
 import { Unit } from "../../lib/unit";
 import useResourceBarValues, { ResourceBarType } from "../../hooks/useResourceBarValues";
 import { Stack } from "@cloudoperators/juno-ui-components/index";
-import { getBarLabel, getEmptyBarLabel, hasAnyBarValues } from "./resourceBarUtils";
+import { getBarLabel, locateAnyAZ, getEmptyBarLabel, hasAnyBarValues } from "./resourceBarUtils";
 import ResourceInfo from "./ResourceInfo";
 
 const barConainer = `
@@ -30,23 +30,25 @@ const extraBaseStyle = `bg-theme-background-lvl-4`;
 const extraFillStyle = `bg-sap-purple-2`;
 
 const ResourceBarBuilder = (props) => {
-  const { parent, resource, unit: unitName, barType, isEditableResource } = { ...props };
+  const { resource, az, unit: unitName, barType, isEditableResource } = { ...props };
   const { showToolTip = false, displayResourceInfo = false } = { ...props };
+  const currentResource = az ? az : resource;
   const unit = new Unit(unitName || "");
-  const { leftBar, rightBar } = useResourceBarValues(resource, barType);
+  const { leftBar, rightBar } = useResourceBarValues(currentResource, barType);
   const isGranular = barType === ResourceBarType.granular;
   const hasLeftBar = hasAnyBarValues(leftBar);
   const hasRightBar = hasAnyBarValues(rightBar);
   const isEmptyBar = !hasLeftBar && !hasRightBar;
-  const paygStyle = { base: hasLeftBar && extraBaseStyle, filled: isEditableResource && extraFillStyle };
+  const isEmptyBarWithBaseQuota = isEmptyBar && locateAnyAZ(resource);
 
+  const paygStyle = { base: hasLeftBar && extraBaseStyle, filled: isEditableResource && extraFillStyle };
   function getResourceBarLabel(bar) {
     if (isEmptyBar) {
-      return getEmptyBarLabel(parent, resource);
+      return getEmptyBarLabel(resource, isGranular);
     }
 
     const hideLabelInfo = bar === rightBar && hasLeftBar;
-    const barLabel = !hideLabelInfo && getBarLabel(resource);
+    const barLabel = !hideLabelInfo && getBarLabel(currentResource);
     return (
       <span className={`progress-bar-label font-bold ${isGranular && "text-xs"}`}>
         {unit.format(bar.utilized) + "/" + unit.format(bar.available)} {<span className="font-normal">{barLabel}</span>}
@@ -94,8 +96,8 @@ const ResourceBarBuilder = (props) => {
           toolTip={showToolTip && getResourceBarToolTip(rightBar)}
         />
       </Stack>
-      {displayResourceInfo && !isEmptyBar && (
-        <ResourceInfo parent={parent} resource={resource} leftBar={leftBar} rightBar={rightBar} unit={unit} />
+      {displayResourceInfo && (!isEmptyBar || isEmptyBarWithBaseQuota) && (
+        <ResourceInfo resource={resource} az={az} leftBar={leftBar} rightBar={rightBar} unit={unit} />
       )}
     </>
   );
