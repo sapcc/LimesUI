2aea665a8ad28267a9a258d02e9038be91c5c49a
VoigtS <s.voigt@sap.com>
2025-09-30 15:01:32 +0200

    --allow-empty

---
+++ .github/scripts/build-app.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# collect the necessary information
+entry_file=$(jq -r '.module // .main // .exports["."]' $PACKAGE_PATH/package.json)
+build_folder=$(dirname $entry_file)
+
+# Run build using turbo
+npm install
+npm run build
+
+# Copy build folder to deploy path
+mkdir -p "$DEPLOY_PATH/$TARGET_FOLDER"
+cp -r "$PACKAGE_PATH/$build_folder/." "$DEPLOY_PATH/$TARGET_FOLDER"
