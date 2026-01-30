332c3e804d204c5599889aec4feef906b7004992
Stefan Voigt <52136954+VoigtS@users.noreply.github.com>
2026-01-30 11:50:00 +0100

    Update .github/scripts/build-app.sh
    Co-authored-by: Stefan Majewsky <majewsky@gmx.net>

--- .github/scripts/build-app.sh
+++ .github/scripts/build-app.sh
@@ -7,32 +7,18 @@ npm run build
 
 # Define appProps for each UI view
 mkdir -p ./public/config
-project_config='{
-  "theme": "theme-light",
-  "projectID": "123",
-  "domainID": "456",
-  "token": "token",
-  "canEdit": true,
-  "mockAPI": true
-}'
-
-domain_config='{
-  "theme": "theme-light",
-  "domainID": "456",
-  "token": "token",
-  "canEdit": true,
-  "mockAPI": true
-}'
-
-cluster_config='{
+base_config='{
   "theme": "theme-light",
   "token": "token",
   "canEdit": true,
   "mockAPI": true
 }'
-echo "$project_config" | jq '.' > ./public/config/project-appProps.json
-echo "$domain_config" | jq '.' > ./public/config/domain-appProps.json
-echo "$cluster_config" | jq '.' > ./public/config/cluster-appProps.json
+cluster_config="$(echo "$base_config" | jq .)"
+echo "$cluster_config" > ./public/config/cluster-appProps.json
+domain_config="$(echo "$cluster_config" | jq '.domainID = "456"')"
+echo "$domain_config" > ./public/config/domain-appProps.json
+project_config="$(echo "$domain_config" | jq '.projectID = "123"')"
+echo "$project_config" > ./public/config/project-appProps.json
 
 # Copy build folder to deploy path
 mkdir -p "$DEPLOY_PATH/$TARGET_FOLDER"
