d7462315e6577fcd7c8628ee645e3b501367429e
VoigtS <VoigtS@users.noreply.github.com>
2026-04-16 15:25:18 +0000

    Deploy preview for PR 234 🛫

---
+++ pr-preview/pr-234/LimesUI/build/quotaPlugin.js
@@ -0,0 +1,1 @@
+import{a as c}from"./chunk-7WX5JC3P.js";import{a as u}from"./chunk-4C3Y3VQG.js";import{d as t}from"./chunk-RRU7Q3KP.js";var r=t(c()),e=t(u()),o=(n,m={})=>{import("./Usage-KJBTN63C.js").then(p=>{o.root=(0,r.createRoot)(n),o.root.render(e.default.createElement(p.default,m?.props))})},a=()=>o.root&&o.root.unmount();export{o as mount,a as unmount};
