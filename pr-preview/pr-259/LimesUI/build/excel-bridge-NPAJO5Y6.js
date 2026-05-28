b66878b2930303c78a7a239d9adb793f31fe0376
VoigtS <VoigtS@users.noreply.github.com>
2026-05-28 09:44:22 +0000

    Deploy preview for PR 259 🛫

---
+++ pr-preview/pr-259/LimesUI/build/excel-bridge-NPAJO5Y6.js
@@ -0,0 +1,1 @@
+import{a,b,c}from"./chunk-VE6XIQD3.js";import"./chunk-4UABDSDW.js";import"./chunk-YKV54P36.js";import"./chunk-RRU7Q3KP.js";export{b as chartToPdf,c as createWordChartPdfRenderer,a as excelToPdf};
