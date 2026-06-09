4d7e4edd27f3952660a5acec79b0ec232357202f
VoigtS <VoigtS@users.noreply.github.com>
2026-06-09 09:57:32 +0000

    Remove preview for PR 264 🛬

--- pr-preview/pr-264/LimesUI/build/excel-bridge-TLWV4K2Q.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c}from"./chunk-4U43L66V.js";import"./chunk-F5Y72SFQ.js";import"./chunk-5EUIDQQI.js";import"./chunk-RRU7Q3KP.js";export{b as chartToPdf,c as createWordChartPdfRenderer,a as excelToPdf};
