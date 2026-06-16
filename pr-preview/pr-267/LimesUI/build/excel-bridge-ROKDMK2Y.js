1471b484277c2c9a22084f639f22b6c3db5d0955
VoigtS <VoigtS@users.noreply.github.com>
2026-06-16 09:18:56 +0000

    Remove preview for PR 267 🛬

--- pr-preview/pr-267/LimesUI/build/excel-bridge-ROKDMK2Y.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c,d}from"./chunk-22MHELEP.js";import"./chunk-FZBBJ37T.js";import"./chunk-YSPUWPVZ.js";import"./chunk-RRU7Q3KP.js";export{b as chartToPdf,c as createWordChartPdfRenderer,d as createWordLayoutChartPdfRenderer,a as excelToPdf};
--- pr-preview/pr-267/LimesUI/build/workbook.browser-BOYOMDUH.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-KIKGWDWD.js";import"./chunk-FZBBJ37T.js";import"./chunk-YSPUWPVZ.js";import"./chunk-RRU7Q3KP.js";export{a as Workbook};
