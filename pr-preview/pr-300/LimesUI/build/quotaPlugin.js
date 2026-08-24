0daef682ae08a5fcfa272e1f289af52c4caa196b
VoigtS <VoigtS@users.noreply.github.com>
2026-08-24 15:04:15 +0000

    Remove preview for PR 300 🛬

--- pr-preview/pr-300/LimesUI/build/quotaPlugin.js
+++
@@ -1,1 +0,0 @@
-import{a as c}from"./chunk-BWN2QOMC.js";import{a as u}from"./chunk-VRDMHOVA.js";import{d as t}from"./chunk-AEHV6DQX.js";var r=t(c()),e=t(u()),o=(n,m={})=>{import("./Usage-UP2ZZKPL.js").then(p=>{o.root=(0,r.createRoot)(n),o.root.render(e.default.createElement(p.default,m?.props))})},a=()=>o.root&&o.root.unmount();export{o as mount,a as unmount};
